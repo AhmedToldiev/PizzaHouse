08477d7897fd50d4357c43f4ff7474314232905f
Ahmed <toldiev.009@mail.ru>
2024-02-11 21:11:25 +0300

    pizzaBlock

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
+import pizzas from './pizzas.json';
+console.log(pizzas);
 function App() {
   return (
     <div className="wrapper">
@@ -17,7 +19,15 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock/>
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj}
+                // title={obj.title}
+                // price={obj.price}
+                // imageUrl={obj.imageUrl}
+                // sizes={obj.sizes}
+                // types={obj.types}
+              />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 export default function Categories() {
   const [activeIndex, setActiveIndex] = useState(0);
   const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+  
   const clickCategory = (index) => {
     setActiveIndex(index);
   };
