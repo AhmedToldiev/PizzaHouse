e7027dc22bc44f2c6bd30a3c4fa4e1d141e9dec5
Ahmed <toldiev.009@mail.ru>
2024-02-13 19:27:01 +0300

    routes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import { Skeleton } from '../components/PizzaBlock/Skeleton';
+
+export default function Home() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://65cb1d59efec34d9ed86c07b.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => setItems(arr), setIsLoading(false));
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </>
+  );
+}
