9c9edbd22e7b1060886323b10951d4e1c45a922b
Ahmed <toldiev.009@mail.ru>
2024-02-17 15:10:17 +0300

    poisk and pagination done

---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+
+import styles from './Paginatiom.module.scss';
+
+export default function Pagination({ onChangePage }) {
+  return (
+    <ReactPaginate
+      className={styles.root}
+      breakLabel="..."
+      nextLabel=">"
+      onPageChange={(e) => onChangePage(e.selected + 1)}
+      pageRangeDisplayed={4}
+      pageCount={3}
+      previousLabel="<"
+      renderOnZeroPageCount={null}
+    />
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,14 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import { Skeleton } from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
 
 export default function Home({ search }) {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+
   const [sortType, setSortType] = useState({
     name: 'популярности',
     sort: 'rating',
@@ -19,15 +22,15 @@ export default function Home({ search }) {
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     setIsLoading(true);
     fetch(
-      `https://65cb1d59efec34d9ed86c07b.mockapi.io/items?${category}&sortBy=${sortby}&order=${order}`,
+      `https://65cb1d59efec34d9ed86c07b.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortby}&order=${order}`,
     )
       .then((res) => res.json())
       .then((arr) => setItems(arr), setIsLoading(false));
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType,currentPage]);
 
   const pizzas = items
     .filter((obj) => {
-      if (obj.title.includes(search)) {
+      if (obj.title.toLowerCase().includes(search.toLowerCase())) {
         return true;
       }
       return false;
@@ -43,6 +46,7 @@ export default function Home({ search }) {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage ={(number)=>setCurrentPage(number)}/>
     </div>
   );
 }
